5d95f057272f789547a13cb85198e86594922449
peke2 <abem@dc4.so-net.ne.jp>
2018-03-21 15:01:45 +0900

    振動を再生するテストを作成。Androidネイティブ。

---
+++ app/src/main/java/xyz/peke2/androidvibrationtest/MainActivity.java
@@ -0,0 +1,86 @@
+package xyz.peke2.androidvibrationtest;
+
+import android.app.Application;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Parcel;
+import android.os.UserHandle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+import android.util.*;
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonStart = findViewById(R.id.button_vib_start);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d("vibration_test", "start");
+                Context context = getApplicationContext();
+
+                Vibrator vib = (Vibrator)context.getSystemService(Service.VIBRATOR_SERVICE);
+                long[] timings = new long[]{100,100,100,100,100,100,100,100,100,100};
+                int[] amplitudes = new int[]{250,10,180,200,0,120,180,30,30,200};
+                VibrationEffect effect = VibrationEffect.createWaveform(timings, amplitudes,-1);
+
+                vib.cancel();
+
+                long st, elapsed;
+                st = System.currentTimeMillis();
+                vib.vibrate(effect);
+                elapsed = System.currentTimeMillis() - st;
+
+                Log.d("vibration_test", "elapsed:"+ String.valueOf(elapsed)+"ms");
+            }
+        });
+
+        Button buttonStop = findViewById(R.id.button_vib_stop);
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d("vibration_test", "stop");
+                Vibrator vib = (Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
+                vib.cancel();
+            }
+        });
+    }
+}
