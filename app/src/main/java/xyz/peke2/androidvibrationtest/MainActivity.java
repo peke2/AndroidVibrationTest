4f69f47d59b5ccf13cb4139d9bdda0d8a3753f2c
peke2 <abem@dc4.so-net.ne.jp>
2018-05-23 00:10:28 +0900

    振動再生時間が想定とずれる現象の確認を追加。 確かに数秒ずれたりするが、条件がよくわからない。

--- app/src/main/java/xyz/peke2/androidvibrationtest/MainActivity.java
+++ app/src/main/java/xyz/peke2/androidvibrationtest/MainActivity.java
@@ -65,10 +65,37 @@ public class MainActivity extends AppCompatActivity {
                 Context context = getApplicationContext();
 
                 Vibrator vib = (Vibrator)context.getSystemService(Service.VIBRATOR_SERVICE);
-                long[] timings = new long[]{100,100,100,100,100,100,100,100,100,100};
-                int[] amplitudes = new int[]{250,10,180,200,0,120,180,30,30,200};
+                //long[] timings = new long[]{100,100,100,100,100,100,100,100,100,100};
+                //int[] amplitudes = new int[]{250,10,180,200,0,120,180,30,30,200};
 
-                if( false ) {
+                //[スレッドでの振動再生で確認]
+                //どちらも約5.2〜5.4秒ほど振動が再生
+                //左右の手で振動とストップウォッチを開始するので多少の誤差はあるが、
+                //確実に5秒は超える
+                //10秒でも15秒でも同様に0.3〜0.5秒ほどしかずれがない
+
+                //同じ値の振動は2000まで連続できない？？
+                //
+                int n = 3000;   //1500まではいけた　→　20秒を超える再生はできない？
+                long[] timings = new long[n];
+                int[] amplitudes = new int[n];
+                for(int i=0; i<n; i++){
+                    timings[i] = 10;
+                    //amplitudes[i] = 255;      //最初一瞬振動して停止、しばらくしたら少し振動しておしまい
+                    //amplitudes[i] = 255 * (((i & 1) == 1)? 1:0);    //振動が続く、ただし20秒想定で約22秒振動した
+                                                                    //30秒想定で32.64秒くらい
+                    amplitudes[i] = (int)(255 * (10 - i%10) * 0.1f);    //これだとだめ？？？
+                }
+                /*
+                long[] timings = new long[]{15000}; //  1つだけでも20000(20秒)は無理
+                int[] amplitudes = new int[]{255};
+                */
+
+
+
+
+                if( true ) {
+                    //  スレッドを使わない処理
                     VibrationEffect effect = VibrationEffect.createWaveform(timings, amplitudes, -1);
 
                     long st, elapsed;
@@ -88,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else
                 {
+                    //  スレッドで振動を再生
                     long st, elapsed;
 
                     st = System.currentTimeMillis();
